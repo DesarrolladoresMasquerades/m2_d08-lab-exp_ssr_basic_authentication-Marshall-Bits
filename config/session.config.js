ee52c57ac18ad57a7812bd3c7c0e8dec8004d69f
Marcelus <kz_@hotmail.es>
2022-02-14 22:40:31 +0100

    login done

---
+++ config/session.config.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require('express-session');
+const favicon = require("serve-favicon");
+const MongoStore = require('connect-mongo');
+const path = require("path");
+
+module.exports = (app) => {
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }))
+  //cookieParser here
+  app.use(cookieParser())
+
+  app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({
+      mongoUrl: `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@cluster0.4pwhb.mongodb.net/users?retryWrites=true&w=majority`,// || "mongoodb://localhost/cookies"
+      ttl: 24 * 60 * 60
+    }),
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, //one day old
+    }
+  })
+  )
+
+
+  app.set("views", path.join(__dirname, "..", "views"));
+  app.set("view engine", "hbs");
+  app.use(express.static(path.join(__dirname, "..", "public")));
+  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
-
 const saltRounds = 5;
 const bcrypt = require("bcrypt");
 
@@ -17,14 +16,14 @@ router
         // --- Check if form is not empty --- \\
         if (!username || !password) {
             res.render("signup", { errorMessage: "All fields required!" })
-            throw new Error("Validation error");
+            // throw new Error("Validation error");
         }
 
         User.findOne({ username })
             .then((user) => {
                 if (user && user.username) {
                     res.render("signup", { errorMessage: "Sorry, this user is already taken" })
-                    throw new Error("Validation error");
+                    // throw new Error("Validation error");
                 };
             })
 
@@ -36,5 +35,49 @@ router
                 res.redirect("/")
             })
     })
+
+router
+    .route("/login")
+    .get((req, res) => {
+        res.render("login");
+    })
+    .post((req, res) => {
+        const username = req.body.username
+        const password = req.body.password
+
+        if (!username || !password) {
+            res.render("login", { errorMessage: "All fields are required" })
+            throw new Error("Validation Error")
+        }
+
+        User.findOne({ username })
+            .then((user) => {
+                if (!user) {
+                    res.render("login", { errorMessage: "Incorrect Credentials" })
+                    throw new Error("Validation error");
+                }
+
+                const isPwCorrect = bcrypt.compareSync(password, user.password)
+
+                if (isPwCorrect) {
+                    req.session.currentUserId = user._id
+                    res.redirect("/auth/profile")
+                } else {
+                    res.render("login", { errorMessage: "Incorrect credentials" })
+                }
+            })
+            .catch((error) => console.log(error));
+    })
+
+router
+    .get('/profile', (req, res) => {
+        const id = req.session.currentUserId;
+        User.findById(id)
+            .then((user) => res.render('profile', user))
+            .catch((error) => console.log(error));
+    })
+
+router.get('/logout', (req, res) => req.session.destroy((err) => res.redirect('/')))
+
 
 module.exports = router;
