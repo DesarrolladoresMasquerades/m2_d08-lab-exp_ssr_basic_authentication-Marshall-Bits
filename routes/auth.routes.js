84837e92f29cb351e2455360a6552800b74bbed0
Marcelus <kz_@hotmail.es>
2022-02-14 21:48:24 +0100

    Iteration 1

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,9 +6,35 @@ const saltRounds = 5;
 const bcrypt = require("bcrypt");
 
 router
-.route("/signup")
-.get((req,res)=>{
-    res.render("signup");
-})
+    .route("/signup")
+    .get((req, res) => {
+        res.render("signup");
+    })
+    .post((req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        // --- Check if form is not empty --- \\
+        if (!username || !password) {
+            res.render("signup", { errorMessage: "All fields required!" })
+            throw new Error("Validation error");
+        }
+
+        User.findOne({ username })
+            .then((user) => {
+                if (user && user.username) {
+                    res.render("signup", { errorMessage: "Sorry, this user is already taken" })
+                    throw new Error("Validation error");
+                };
+            })
+
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPwd = bcrypt.hashSync(password, salt);
+
+        User.create({ username, password: hashedPwd })
+            .then(() => {
+                res.redirect("/")
+            })
+    })
 
 module.exports = router;
